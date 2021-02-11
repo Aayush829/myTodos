7629ef039f38c01d87b9b1f9ae39df8529f8c837
Aayush Pandey <aayushpandey829@gmail.com>
2021-02-11 13:06:45 +0530

    fifth commit

--- routes/api.js
+++ routes/api.js
@@ -7,8 +7,14 @@ router.get('/', function(req, res, next) {
 });
 
 var addTodo = require('../api/addTodo')
-router.post('/adduser', addUser.addUser)
+router.post('/addTodo', addTodo.addTodo)
 
 var addUser = require('../api/addUser')
 router.post('/addUser', addUser.addUser)
+
+var authenticate = require('../api/authenticate')
+router.post('/authenticate', authenticate.authenticate)
+
+var checkTodo = require('../api/checkTodo')
+router.post('/checkTodo', checkTodo.checkTodo)
 module.exports = router;
