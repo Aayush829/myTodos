faaa2d7e8c45a049b063991354628cdc9dbc85da
Aayush Pandey <aayushpandey829@gmail.com>
2021-02-11 15:40:34 +0530

    tenth commit

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var view = require('./routes/view');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -15,18 +15,25 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(session({
+  secret: 'secret1234',
+  resave: true,
+  saveUnintialized: true
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', view);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err =  new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
@@ -39,5 +46,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000);
 
 module.exports = app;
