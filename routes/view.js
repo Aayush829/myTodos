2c5a2f8e9b20ff3f56d26346f0486041dc661e92
Aayush Pandey <aayushpandey829@gmail.com>
2021-02-11 13:44:25 +0530

    seventh commit

--- routes/view.js
+++ routes/view.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/home', auth, function (req, res, next) {
+  if (req.session.isLogged) {
+  res.render('home', { title: req.session.email })
+  }
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/signin', function  (req, res, next) {
+  res.render('signin');
+});
+
+router.get('/signup', function (req, res,  next) {
+  res.render('signup');
+})
 
 module.exports = router;
